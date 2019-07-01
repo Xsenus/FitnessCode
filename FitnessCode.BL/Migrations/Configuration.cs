222a60e1a63ae7ad8fbb5741261fd9bf212ca5c6
Илья <ilel@list.ru>
2019-07-01 15:52:58 +0700

    Добавлен установщик (Advanced Installer)

--- FitnessCode.BL/Migrations/Configuration.cs
+++ FitnessCode.BL/Migrations/Configuration.cs
@@ -1,9 +1,6 @@
 namespace FitnessCode.BL.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<FitnessCode.BL.Controller.FitnessContext>
     {
